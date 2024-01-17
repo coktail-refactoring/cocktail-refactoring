b5c24cf7e73e912513311fa9fee3548b1a67fe92
soyounRoh <deresa0223@gmail.com>
2024-01-18 03:42:56 +0900

    refactor : refatoed registet Page. css and structure only.

--- coktail/src/pages/recipes/recipeRegister.jsx
+++ coktail/src/pages/recipes/recipeRegister.jsx
@@ -1,21 +1,24 @@
-import styled from 'styled-components'
+import * as Styled from './recipeRegister.style'
 import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { api } from '@/utils/api'
-import Spacing from '@/utils/Spacing'
-import { paddingBottom } from '@/assets/styleVariables'
 
 /* Components */
-import Naming from '@/components/recipeUpload/Naming'
-import RecipeStep from '@/components/recipeUpload/RecipeStep'
+import Title from '@/components/title'
 import ImageUpload from '@/components/recipeUpload/ImageUpload'
 import StarRating from '@/components/recipeUpload/StarRating'
-import Title from '@/components/title'
-import TitleAndTextarea from '@/components/recipeUpload/TitleAndTextarea'
+
+/* Icons */
+import AddBtnIcon from '@/components/icons/AddBtnIcon'
+import CloseIcon from '@/components/icons/CloseIcon'
+
 
 export default function RecipeRegister() {
+  const [baseData, setBaseData] = useState([])
+  const [tag, setTag] = useState('')
+
   const [name, setName] = useState('')
-  const [base, setBase] = useState('')
+  const [base, setBase] = useState('657c3fdc3d11af1efa276d9f')
   const [tags, setTags] = useState([])
   const [images, setImages] = useState([])
   const [abv, setAbv] = useState(0)
@@ -24,9 +27,49 @@ export default function RecipeRegister() {
   const [bitter, setBitter] = useState(0)
   const [description, setDescription] = useState('')
   const [ingredient, setIngredient] = useState('')
-  const [recipeArr, setRecipeArr] = useState([])
+  const [recipeArr, setRecipeArr] = useState([
+    {
+      image: '',
+      content: '',
+    },
+  ])
 
-  //레시피 등록 form Data
+  
+  //baseApi
+  const getBaseData = async () => {
+    const response = await api.get('/bases')
+    const data = response.data.bases
+    setBaseData(data)
+  }
+
+  useEffect(() => {
+    getBaseData()
+  }, [])
+
+  //tagHandler
+  function submitTagHandler(e) {
+    e.preventDefault()
+    const cleanedTag = tag.replace(/[^a-zA-Z가-힣]/g, '')
+    if (cleanedTag.trim() !== '') {
+      setTags([...tags, cleanedTag])
+      setTag('')
+    }
+  }
+
+  function deleteHandler(index) {
+    const newArr = [...tags]
+    newArr.splice(index, 1)
+    setTags(newArr)
+  }
+
+  //recipe Step
+  function addStepHandler(e) {
+    e.preventDefault()
+    const newStepArr = { image: '', content: '' }
+    setRecipeArr([...recipeArr, newStepArr])
+  }
+
+  //form Data
   const submitHandler = async (e, index) => {
     e.preventDefault()
     const formData = new FormData()
@@ -48,7 +91,7 @@ export default function RecipeRegister() {
     })
 
     for (let value of formData.values()) {
-      console.log( "이건 등록할떄 데이터 ", value);
+      console.log('이건 등록할떄 데이터 ', value)
     }
 
     if (!formData) {
@@ -70,13 +113,59 @@ export default function RecipeRegister() {
   return (
     <>
       <Title>DIY레시피 등록</Title>
-      <Container>
-        <RecipeForm onSubmit={submitHandler}>
-          <Naming setName={setName} setBase={setBase} setTagArr={setTags} />
+      <Styled.Container>
+        <Styled.RecipeForm onSubmit={submitHandler}>
+          <div className="gap10">
+            <Styled.InputBox>
+              <Styled.Input
+                type="text"
+                placeholder="칵테일 이름"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            </Styled.InputBox>
+
+            <Styled.Select
+              value={base}
+              onChange={(e) => setBase(e.target.value)}
+            >
+              {baseData.map((i, index) => (
+                <option key={index + i} value={i._id}>
+                  {i.name}
+                </option>
+              ))}
+            </Styled.Select>
+
+            <Styled.InputBox>
+              <Styled.Input
+                type="text"
+                value={tag}
+                onChange={(e) => setTag(e.target.value)}
+                placeholder="해시태그를 입력해주세요"
+              />
+              <Styled.AddTagBtn onClick={submitTagHandler}>
+                <AddBtnIcon width={15} />
+              </Styled.AddTagBtn>
+            </Styled.InputBox>
+            <Styled.TagsList>
+              {tags.map((item, index) => (
+                <li key={item + index} className="tagItem">
+                  <div>
+                    {'#' + item}
+                    <button className="closeBtn" onClick={deleteHandler}>
+                      <CloseIcon width={10} height={10} fill={'#000000'} />
+                    </button>
+                  </div>
+                </li>
+              ))}
+            </Styled.TagsList>
+          </div>
+
           <div className="imgFileWrap">
             <ImageUpload setImgs={setImages} />
           </div>
-          <RatingBox>
+
+          <Styled.RatingBox>
             <div className="ratingWrap">
               <div>도수</div>
               <StarRating setRating={(r) => setAbv(r)} />
@@ -93,69 +182,53 @@ export default function RecipeRegister() {
               <div>쓴맛</div>
               <StarRating setRating={(r) => setBitter(r)} />
             </div>
-          </RatingBox>
-          <TitleAndTextarea title="소개글" setContent={setDescription} />
-          <TitleAndTextarea title="재료" setContent={setIngredient} />
+          </Styled.RatingBox>
 
-          <RecipeStep setRecipes={setRecipeArr} />
-          {/* <RecipeStep setRecipes={setRecipes} setRecipeImages={setRecipeImages} /> */}
-          <ButtonGroup>
+          <Styled.TitleAndTextArea>
+            <Styled.TitleBadge>소개글</Styled.TitleBadge>
+            <Styled.Textarea
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+              placeholder="내용을 입력해주세요 :)"
+              required
+            ></Styled.Textarea>
+          </Styled.TitleAndTextArea>
+          
+          <Styled.TitleAndTextArea>
+            <Styled.TitleBadge>재료</Styled.TitleBadge>
+            <Styled.Textarea
+              value={ingredient}
+              onChange={(e) => setIngredient(e.target.value)}
+              placeholder="재료를 입력해주세요 :)"
+              required
+            ></Styled.Textarea>
+          </Styled.TitleAndTextArea>
+
+          <Styled.Recipe>
+            <Styled.TitleBadge>레시피</Styled.TitleBadge>
+            {recipeArr.map((item, index) => (
+              <Styled.Step key={index+item}>
+                <Styled.Textarea placeholder={`${index + 1}. 설명을 입력해 주세요`} />
+                <div className="imgBox">
+                  <ImageUpload />
+                </div>
+              </Styled.Step>
+            ))}
+            <div className="btnBox">
+              <Styled.AddRecipeBtn onClick={addStepHandler}>+ 추가</Styled.AddRecipeBtn>
+            </div>
+          </Styled.Recipe>
+
+          <Styled.ButtonGroup>
             <button className="btn cancelBtn" onClick={cancelHandler}>
               취소
             </button>
             <button className="btn submitBtn" type="submit">
               등록하기
             </button>
-          </ButtonGroup>
-        </RecipeForm>
-        <Spacing size={paddingBottom} />
-      </Container>
+          </Styled.ButtonGroup>
+        </Styled.RecipeForm>
+      </Styled.Container>
     </>
   )
 }
-
-const Container = styled.div`
-  padding: 0 2em;
-`
-
-const RecipeForm = styled.form`
-  display: flex;
-  flex-direction: column;
-  gap: 20px;
-  .imgFileWrap {
-    height: 248px;
-  }
-`
-const RatingBox = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-evenly;
-  gap: 22px;
-  .ratingWrap {
-    display: flex;
-    align-items: center;
-    gap: 1rem;
-  }
-`
-
-const ButtonGroup = styled.div`
-  display: flex;
-  justify-content: flex-end;
-  gap: 10px;
-  .btn {
-    border: none;
-    box-shadow: none;
-    border-radius: 20rem;
-    padding: 0;
-    overflow: visible;
-    cursor: pointer;
-    width: 100px;
-    height: 36px;
-  }
-  .cancelBtn {
-    background-color: #d5d5d5;
-  }
-  .submitBtn {
-    background-color: #b0d96d;
-  }
-`
---
+++ coktail/src/pages/recipes/recipeRegister.style.js
@@ -0,0 +1,157 @@
+import styled from 'styled-components'
+import { primaryGray, secondGray, deepGreen } from '@/assets/styleVariables'
+
+
+export const Container = styled.div`
+  padding: 0 24px 80px 24px;
+`
+
+export const RecipeForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 28px;
+  .gap10 {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+  }
+  .imgFileWrap {
+    height: 204px;
+  }
+`
+export const InputBox = styled.div`
+  position: relative;
+`
+export const Input = styled.input`
+  width: 100%;
+  min-height: 38px;
+  border: 2px solid #d5d5d5;
+  border-radius: .6rem;
+  padding: 0 8px;
+  &:focus {
+    border: 2px solid #797979;
+    outline: none;
+  }
+`
+
+export const Select = styled.select`
+  width: 100%;
+  min-height: 38px;
+  border: 2px solid #d5d5d5;
+  border-radius: .6rem;
+`
+
+export const AddTagBtn = styled.button`
+  position: absolute;
+  right: 8px;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  top: 0;
+`
+
+export const TagsList = styled.ul`
+  display: flex;
+  margin-top: -8px;
+  flex-wrap: wrap;
+  gap: 5px;
+  .tagItem {
+    div {
+      border-radius: 2rem;
+      background-color: ${deepGreen};
+      padding: 3px 12px;
+      color: #000;
+      display: flex;
+      align-items: center;
+      gap: 4px;
+    }
+    .closeBtn {
+      background: none;
+      border: none;
+      box-shadow: none;
+      border-radius: 0;
+      padding: 0;
+      overflow: visible;
+      cursor: pointer;
+    }
+  }
+`
+export const RatingBox = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+  gap: 22px;
+  .ratingWrap {
+    display: flex;
+    align-items: center;
+    gap: 10px;
+  }
+`
+export const TitleAndTextArea = styled.div`
+  position: relative;
+`
+
+export const TitleBadge = styled.div`
+  display: inline-block;
+  border-radius: 2rem;
+  background-color: #b0d96d;
+  padding: 6px 15px;
+`
+
+export const Textarea = styled.textarea`
+  width: 100%;
+  height: 80px;
+  resize: none;
+  border: 2px solid #d5d5d5;
+  border-radius: .6rem;
+  margin-top: 8px;
+  white-space: pre-wrap;
+  word-break: break-all;
+  &:focus {
+    border-color: #797979;
+    outline: none;
+  }
+`
+
+export const Recipe = styled.div`
+  .btnBox {
+    display: flex;
+    justify-content: center;
+  }
+`
+export const Step = styled.div`
+  .imgBox {
+    margin-top: 8px;
+    height: 204px;
+  }
+`
+
+export const AddRecipeBtn = styled.button`
+  margin-top: 8px;
+  font-size: 1rem;
+  padding: 6px 15px;
+  background-color: ${primaryGray};
+  border-radius: 2rem;
+`
+
+export const ButtonGroup = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  gap: 10px;
+  .btn {
+    border: none;
+    box-shadow: none;
+    border-radius: 2rem;
+    padding: 0;
+    overflow: visible;
+    cursor: pointer;
+    width: 100px;
+    height: 36px;
+  }
+  .cancelBtn {
+    background-color: ${secondGray};
+  }
+  .submitBtn {
+    background-color: ${deepGreen};
+  }
+`
